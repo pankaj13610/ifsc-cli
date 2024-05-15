9b14922b425f00b2591f48985727da2b616a45a5
Pankaj Kumar Singh <pankaj.singh@shiprocket.com>
2024-05-15 17:14:36 +0530

    Call ifsc api and show branchname and city name for valid codes

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ This a demo cli app to fetch details of IFSC code from Razorpay IFSC API
 
 This is made for learning GoLang. 
 
+### Steps to run 
+` go run main.go { IFSC Code } `
+
+Please give a ifsc code and you will get branch name and city name for that code e.g
+
+` go run main.go SBIN0012892 `
+
--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello from Golang")
+	codes := os.Args[1:]
+	if len(codes) != 1 {
+		fmt.Fprintf(os.Stderr, "Please give exactly 1 ifsc code as argument \n")
+		os.Exit(1)
+	}
+	getData(codes[0])
+}
+
+func getData(ifsc string) {
+	requestURL := fmt.Sprintf("https://ifsc.razorpay.com/%s", ifsc)
+	req, err := http.NewRequest(http.MethodGet, requestURL, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not create request: %s \n", err)
+		os.Exit(1)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error making http request %s \n", err)
+		os.Exit(1)
+	}
+
+	if res.StatusCode != 200 {
+		fmt.Fprintln(os.Stderr, "Invalid IFSC Code")
+		os.Exit(1)
+	}
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "client: could not read response body: %s\n", err)
+		os.Exit(1)
+	}
+	var j map[string]interface{}
+	err = json.Unmarshal(resBody, &j)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to decode json %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(j["BRANCH"], j["CITY"])
 }
